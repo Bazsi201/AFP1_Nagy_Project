40d077a6f39290602a5e73f4311c2dca5798a14b
szlzs05 <zsoltisw@gmail.com>
2022-11-18 12:52:08 +0100

    questionnaire\create view form kialakítása\Title label, input, small tagek

--- Questionnaire/resources/views/questionnaire/create.blade.php
+++ Questionnaire/resources/views/questionnaire/create.blade.php
@@ -8,7 +8,16 @@
                 <div class="card-header">{{ __('Create new questionnaire') }}</div>
 
                 <div class="card-body">
+                    <form action="/questionnaires" method="post">
                     
+                        @csrf
+
+                        <div class="form-group">
+                            <label for="title">Title</label>
+                            <input name="title" type="text" class="form-control" id="title" aria-describedby="title" placeholder="Enter title">
+                            <small id="titleHelp" class="form-text text-muted">Give your questionnaire a title that attracts attention.</small>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
