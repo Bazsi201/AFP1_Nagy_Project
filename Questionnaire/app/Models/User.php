b813600ad90af70a6392eccff0df00ea6a6c96a7
szlzs05 <zsoltisw@gmail.com>
2022-11-18 13:30:58 +0100

    User model questionnaires relationship elkészítése

--- Questionnaire/app/Models/User.php
+++ Questionnaire/app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function questionnaires() {
+        return $this->hasMany(\App\Models\Questionnaire::class);
+    }
 }
