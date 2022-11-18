71ff134f97cd940a4c90d77651c1e50422b135e7
szlzs05 <zsoltisw@gmail.com>
2022-11-18 12:40:09 +0100

    QuestionnaireController létrehozása, create function létrehozása

---
+++ Questionnaire/app/Http/Controllers/QuestionnaireController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class QuestionnaireController extends Controller
+{
+    public function create() {
+        return view('questionnaire.create');
+    }
+}
