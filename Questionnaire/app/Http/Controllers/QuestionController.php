403396c8998e84dc79f31394dd8737cd239d1ad0
kmartin001 <kaponyamartin@gmail.com>
2022-11-20 13:37:30 +0100

    QuestionController store method validation létrehozása

--- Questionnaire/app/Http/Controllers/QuestionController.php
+++ Questionnaire/app/Http/Controllers/QuestionController.php
@@ -9,4 +9,12 @@ class QuestionController extends Controller
     public function create(\App\Models\Questionnaire $questionnaire) {
         return view('question.create', compact('questionnaire'));
     }
+
+    public function store(\App\Models\Questionnaire $questionnaire) {
+        $data = request()->validate([
+            'question.question' => 'required',
+            'answers.*.answer' => 'required',
+        ]);
+
+    }
 }
