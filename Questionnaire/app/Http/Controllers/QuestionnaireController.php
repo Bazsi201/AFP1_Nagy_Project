cc8915a6a0a3291da56e07c873bf905e4b4e8588
Bazsi201 <m.bazsi201@gmail.com>
2022-11-20 14:10:49 +0100

    QuestionnaireController show method load relations hozzáadása

--- Questionnaire/app/Http/Controllers/QuestionnaireController.php
+++ Questionnaire/app/Http/Controllers/QuestionnaireController.php
@@ -27,6 +27,7 @@ class QuestionnaireController extends Controller
     }
 
     public function show(\App\Models\Questionnaire $questionnaire) {
+        $questionnaire->load('questions.answers');
         return view('questionnaire.show', compact('questionnaire'));
     }
 }
