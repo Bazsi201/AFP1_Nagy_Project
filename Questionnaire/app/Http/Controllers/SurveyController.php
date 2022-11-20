250542eacf8f0b96ce62034c06ae5301609c3d95
Bazsi201 <m.bazsi201@gmail.com>
2022-11-20 14:28:44 +0100

    SurveyController show method létrehozása, return view

--- Questionnaire/app/Http/Controllers/SurveyController.php
+++ Questionnaire/app/Http/Controllers/SurveyController.php
@@ -1,10 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Questionnaire;
+
 
 use Illuminate\Http\Request;
 
 class SurveyController extends Controller
 {
-    //
+    public function show(Questionnaire $questionnaire, $slug) {
+        return view('survey.show', compact('questionnaire'));
+    }
 }
