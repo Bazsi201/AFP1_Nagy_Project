19f075cdbfbd972827109769ba0ec58e10a02dd4
Bazsi201 <m.bazsi201@gmail.com>
2022-11-20 14:31:56 +0100

    SurveyController load relations hozzáadása

--- Questionnaire/app/Http/Controllers/SurveyController.php
+++ Questionnaire/app/Http/Controllers/SurveyController.php
@@ -9,6 +9,9 @@ use Illuminate\Http\Request;
 class SurveyController extends Controller
 {
     public function show(Questionnaire $questionnaire, $slug) {
+
+        $questionnaire->load('questions.answers');
+
         return view('survey.show', compact('questionnaire'));
     }
 }
