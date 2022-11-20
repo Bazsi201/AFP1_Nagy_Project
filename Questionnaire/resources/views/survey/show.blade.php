b9b365706f533db41cafa9b5ab818f6f5804e307
Bazsi201 <m.bazsi201@gmail.com>
2022-11-20 14:40:43 +0100

    survey\show view kérdések, lehetséges válaszok listázása

--- Questionnaire/resources/views/survey/show.blade.php
+++ Questionnaire/resources/views/survey/show.blade.php
@@ -10,6 +10,22 @@
             <form action="#" method="post">
                 @csrf
                 
+                @foreach ($questionnaire->questions as $key => $question)
+                    <div class="card mt-4">
+                        <div class="card-header"><strong>{{ $key + 1 }}</strong> {{ $question->question }}</div>
+                            <div class="card-body">
+                                <ul class="list-group">
+                                    @foreach ($question->answers as $answer)
+                                            <li class="list-group-item">
+                                                {{ $answer->answer }}
+                                            </li>
+                                        </label>
+                                    @endforeach
+                                </ul>
+                            </div>
+                    </div>
+                @endforeach
+
             </form>
         </div>
     </div>
