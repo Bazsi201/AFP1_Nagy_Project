036da8c8baac4c3666bc734c18153e9d5384e683
Bazsi201 <m.bazsi201@gmail.com>
2022-11-20 15:16:41 +0100

    survey\show view Your information card hozzáadása

--- Questionnaire/resources/views/survey/show.blade.php
+++ Questionnaire/resources/views/survey/show.blade.php
@@ -36,6 +36,12 @@
                     </div>
                 @endforeach
 
+                <div class="card mt-4">
+                    <div class="card-header">Your information</div>
+                        <div class="card-body">
+                            
+                        </div>
+                </div>
             </form>
         </div>
     </div>
