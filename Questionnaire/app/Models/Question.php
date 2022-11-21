cca875e610fc1c729ba8b710d9f6196e195f59fd
kmartin001 <kaponyamartin@gmail.com>
2022-11-21 15:21:05 +0100

    Question model responses relationship function megírása

--- Questionnaire/app/Models/Question.php
+++ Questionnaire/app/Models/Question.php
@@ -18,4 +18,9 @@ class Question extends Model
     public function answers() {
         return $this->hasMany(Answer::class);
     }
+
+    public function responses() 
+    {
+        return $this->hasMany(SurveyResponse::class);
+    }
 }
